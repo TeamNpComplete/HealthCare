8db18f787ca70182e170f83803c57939082a88e7
Imperial-Dragon <adityagunjal1365@gmail.com>
2020-04-09 18:41:25 +0530

    initialized empty project

---
+++ web/config.php
@@ -0,0 +1,19 @@
+<html>
+   <head>
+      <title>Connecting MySQL Server</title>
+   </head>
+   <body>
+      <?php
+         $dbhost = 'remotemysql.com:3306';
+         $dbuser = 'AbT5ydOVVn';
+         $dbpass = '1nN7Vx4PQ3';
+         $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+         
+         if(! $conn ) {
+            die('Could not connect: ' . mysqli_error());
+         }
+         echo 'Connected successfully';
+         mysqli_close($conn);
+      ?>
+   </body>
+</html>
